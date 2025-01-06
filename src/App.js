79f15bdea34ecd452842c4a4483eced81661de4d
0and4 <iwonchu215@gmail.com>
2025-01-06 17:48:22 +0900

    index 코드 작성 - 상단 메뉴바

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import avatar from "./images/Avatar.png";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+import Gallery from "./component/Gallery";
+import FAQ from "./component/FAQ";
+import AboutUs from "./component/AboutUs";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <nav>
+            <p id="logo">withOcean</p>
+            <ul className="menu">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/gallery">Gallery</Link>
+              </li>
+              <li>
+                <Link to="/faq">FAQ</Link>
+              </li>
+              <li>
+                <Link to="/about-us">About Us</Link>
+              </li>
+            </ul>
+            <div className="avatar">
+              <img alt="avatar" src={avatar}></img>
+            </div>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/about-us" element={<AboutUs />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
+function Home() {
+  return (
+    <div>
+      <h2>main</h2>
+    </div>
+  );
+}
 export default App;
